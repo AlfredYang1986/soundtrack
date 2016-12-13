4e7099acb374b14cb8e452e9e6175764f11232a9
AlfredYang1986 <yangyuanpig@gmail.com>
2016-12-13 09:20:29 +0800

    auth

--- app/module/auth/AuthModule.scala
+++ app/module/auth/AuthModule.scala
@@ -2,6 +2,7 @@ package module.auth
 
 import pattern.ModuleTrait
 import play.api.libs.json.JsValue
+import play.api.libs.json.Json.toJson
 import soundtrackmessages.MessageDefines
 import AuthMessages._
 
@@ -9,6 +10,17 @@ import util.dao.from
 import util.dao._data_connection
 import util.errorcode.ErrorCode
 import com.mongodb.casbah.Imports._
+
+import util.errorcode.ErrorCode
+import module.sercurity.Sercurity
+import java.util.Date
+
+object AuthStatus {
+	object normal extends AuthDefines(0, "normal")
+	object authed extends AuthDefines(1, "auth")
+}
+
+sealed case class AuthDefines(t : Int, d : String)
 
 object AuthModule extends ModuleTrait {
 
@@ -22,26 +34,85 @@ object AuthModule extends ModuleTrait {
 	}
 	
 	def authCreateUser(data : JsValue) : (Option[Map[String, JsValue]], Option[JsValue]) = {
-		null
+		try {
+			val obj = Js2DBObject(data)
+			_data_connection.getCollection("auth") += obj
+			(Some(DB2JsValue(obj)), None)
+			
+		} catch {
+			case ex : Exception => (None, Some(ErrorCode.errorToJson("wrong input")))
+		}
 	}
 	
 	def authCheck(data : JsValue) : (Option[Map[String, JsValue]], Option[JsValue]) = {
-		null
+		try {
+			val token = (data \ "token").asOpt[String].map (x => x).getOrElse(throw new Exception("wrong input"))
+			val least_auth = (data \ "least_auth").asOpt[Int].map (x => x).getOrElse(throw new Exception("wrong input"))
+		
+			(from db() in "auth" where ("token" -> token) select (DB2JsValue(_))).toList match {
+				case head :: Nil => {
+					if (head.get("auth").get.asOpt[Int].get < least_auth) throw new Exception("not allowed")
+					else (Some(head), None)
+				}
+				case _ => throw new Exception("unknown user")
+			}
+			
+		} catch {
+			case ex : Exception => (None, Some(ErrorCode.errorToJson("wrong input")))
+		}
 	}
 	
 	def queryUser(data : JsValue) : (Option[Map[String, JsValue]], Option[JsValue]) = {
-		null
+		try {
+			val wechat_id = (data \ "wechat_id").asOpt[String].map (x => x).getOrElse(throw new Exception("wrong input"))
+			
+			(from db() in "auth" where ("wechat_id" -> wechat_id) select (DB2JsValue(_))).toList match {
+				case head :: Nil => (Some(head), None)
+				case _ => throw new Exception("unknown user")
+			}
+			
+		} catch {
+			case ex : Exception => (None, Some(ErrorCode.errorToJson("wrong input")))
+		}
 	}
 	
 	def authUpdateUser(data : JsValue) : (Option[Map[String, JsValue]], Option[JsValue]) = {
-		null
+		try {
+			val user_id = (data \ "user_id").asOpt[String].map (x => x).getOrElse(throw new Exception("wrong input"))
+
+			(from db() in "auth" where ("user_id" -> user_id) select (DB2JsValue(_))).toList match {
+				case head :: Nil => (Some(head), None)
+				case _ => throw new Exception("unknown user")
+			}
+			
+		} catch {
+			case ex : Exception => (None, Some(ErrorCode.errorToJson("wrong input")))
+		}
 	}
 	
 	def Js2DBObject(data : JsValue) : MongoDBObject = {
-		null
+		
+		val builder = MongoDBObject.newBuilder
+		val wechat_id = (data \ "wechat_id").asOpt[String].map (x => x).getOrElse(throw new Exception("wrong input"))
+		builder += "wechat_id" -> (data \ "wechat_id").asOpt[String].map (x => x).getOrElse(throw new Exception("wrong input"))
+
+		builder += "screen_name" -> (data \ "screen_name").asOpt[String].map (x => x).getOrElse(throw new Exception("wrong input"))
+		builder += "screen_photo" -> (data \ "screen_photo").asOpt[String].map (x => x).getOrElse(throw new Exception("wrong input"))
+		builder += "gender" -> (data \ "gender").asOpt[Int].map (x => x).getOrElse(throw new Exception("wrong input"))
+		
+		val d = new Date().getTime
+		builder += "token" -> Sercurity.md5Hash(wechat_id + Sercurity.getTimeSpanWithMillSeconds)
+
+		builder += "start_in" -> -1
+		builder += "expired_in" -> -1
+		builder += "register" -> d
+		builder += "auth" -> AuthStatus.normal
+		
+		builder.result
 	}
 	
 	def DB2JsValue(x : MongoDBObject) : Map[String, JsValue] = {
-		null
+		Map("wechat_id" -> toJson(x.getAs[String]("wechat_id").get),
+			"screen_name" -> toJson(x.getAs[String]("screen_name").get))
 	}
 }
