0aee238147c7d691bf4442155c5c2308fab50150
AlfredYang1986 <yangyuanpig@gmail.com>
2016-12-19 23:49:45 +0800

    界面框架

---
+++ app/controllers/pages/HomeController.scala
@@ -0,0 +1,15 @@
+package controllers.pages
+
+import play.api._
+import play.api.mvc._
+
+import play.api.libs.json.JsValue
+import play.api.libs.json.Json.toJson
+
+object HomeController extends Controller {
+	def index = Action {
+		Ok(views.html.index("soundtrack")
+				(toJson(Map("name" -> "abcde")))
+				(toJson(Map("name" -> "abcde")) :: toJson(Map("name" -> "abcde")) :: Nil))
+	}
+}
